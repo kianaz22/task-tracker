fbfd5f573314281f1bf57cf2b9af7a52ec7c8a30
unknown <kiana.zrb@gmail.com>
2021-09-02 23:14:04 +0430

    heading colors

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { FaArrowLeft, FaArrowRight, FaCalendarAlt } from 'react-icons/fa'
 
 const Task = ({ task, deleteTask, changeStatus,status }) => {
+    const [details, setDetails] = useState(true)
+    const toggleDetails = () => {
+        setDetails(!details)
+    }
     return (
-        <div>
             <div className='task'>
-                <h3>{task.text}</h3>
-                <p className='details'>{task.details}</p>
+                <h3 onClick={toggleDetails}>{task.text}</h3>
+                {details && <p className='details'>{task.details}</p>}
                 <div className='time'>
                     <FaCalendarAlt /> 
                     <p>{task.time}</p>
@@ -18,7 +21,6 @@ const Task = ({ task, deleteTask, changeStatus,status }) => {
                     {status==='running' && <button className='finished-btn' onClick={() => changeStatus(task.id,'finished')}>finished&nbsp;<FaArrowRight /></button>}
                 </div>
             </div>
-        </div>
     )
 }
 
